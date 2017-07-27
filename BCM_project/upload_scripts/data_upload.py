c3dedb830e889b0098687884be90afca88ec0a9e
jvrakor <jost@genialis.com>
2017-07-27 15:57:13 +0200

    Modify upload and annotate script and add validation

--- BCM_project/upload_scripts/data_upload.py
+++ BCM_project/upload_scripts/data_upload.py
@@ -1,126 +1,310 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+
 import argparse
-import os
 import csv
+import glob
+import os
 import resdk
-from resdk.resources import Collection
+import xlrd
+
+COLUMNS = {
+    'SAMPLE_NAME',
+    'FASTQ_R1',
+    'FASTQ_R2',
+    'SEQ_TYPE',
+    'COLLECTION',
+    'ANNOTATOR',
+    'SOURCE',
+    'ORGANISM',
+    'CELL_TYPE',
+    'STRAIN',
+    'TISSUE',
+    'AGE',
+    'GENOTYPE',
+    'MOLECULE',
+    'LIBRARY_STRATEGY',
+    'EXTRACTION_PROTOCOL',
+    'GROWTH_PROTOCOL',
+    'TREATMENT_PROTOCOL',
+    'LIBRARY_CONSTRUCTION_PROTOCOL',
+    'BARCODE',
+    'ANTIBODY',
+    'FACILITY',
+    'OTHER_CHAR_1',
+    'OTHER_CHAR_2',
+}
+
+ORGANISM = {
+    'Homo sapiens',
+    'Mus musculus',
+    'Dictyostelium discoideum',
+    'Rattus norvegicus',
+}
+
+MOLECULE = {
+    'total RNA',
+    'polyA RNA',
+    'cytoplasmic RNA',
+    'nuclear RNA',
+    'genomic DNA',
+    'protein',
+    'other',
+}
+
+OPTIONAL = {
+    'LIBRARY_STRATEGY',
+    'TISSUE',
+    'AGE',
+    'OTHER_CHAR_1',
+    'OTHER_CHAR_2',
+}
+
+class FileImporter(object):
+    """Import annotation spreadsheet."""
+
+    def __init__(self, annotation_path):
+        self.sample_list = []
+        self.path = annotation_path
+        self.populate_samples()
+        self.validate()
+
+    def extension(self):
+        """Find spreadsheet file extension."""
+        return os.path.splitext(self.path)[1]
+
+    def _read_xlrd(self):
+        """Read Excel spreadsheet annotation file."""
+        workbook = xlrd.open_workbook(self.path)
+        worksheet = workbook.sheets()[0]
+        header = worksheet.row_values(0)
+        for rownum in range(1, worksheet.nrows):
+            row = worksheet.row_values(rownum)
+            entries = {}
+            for i, value in enumerate(row):
+                if isinstance(value, float):
+                    entries[header[i]] = str(value)
+                else:
+                    entries[header[i]] = value
+            self.sample_list.append(entries)
+
+    def _read_text_file(self):
+        """Read simple spreadsheet annotation file."""
+        with open(self.path, 'rb') as sample_sheet:
+            self.sample_list = list(csv.DictReader(sample_sheet, delimiter='\t'))
+
+    def populate_samples(self):
+        """Check the format of annotation file and asign read function."""
+        if self.extension() in ['.xls', '.xlsx', '.xlsm']:
+            self._read_xlrd()
+        elif self.extension() in ['.txt', '.tab', '.tsv']:
+            self._read_text_file()
+        else:
+            raise TypeError(
+                "Annotation spreadsheet extension `{}` not recognised."
+                " Options are: `.xls`, `.xlsx`, `.xlsm`, `.txt`, "
+                "`.tab`, `.tsv`.".format(self.extension())
+            )
+
+    def validate(self):
+        """Validate the annotation spreadsheet file."""
+        for sample in self.sample_list:
+            diff1 = COLUMNS - set(sample.keys())
+            diff2 = set(sample.keys()) - COLUMNS
+            for var_name, VAR in [('organism', ORGANISM), ('molecule', MOLECULE)]:
+                var = sample[var_name.upper()]
+                if var and var not in VAR:
+                    raise ValueError(
+                        "`{0}` is not a valid {1}. Valid"
+                        " {2}s are found in Annotation_spreadsheet"
+                        ".xlsm sheet Options. Spreadsheet can be found on "
+                        "https://github.com/genialis/resdk-scripts/tree/master"
+                        "/BCM_project/upload_scripts.".format(var, var_name.upper(), var_name)
+                    )
+            err_msg = (
+                "Headers `{0}` {1}. You should use the"
+                " headers from Annotation_spreadsheet.xlsm found on "
+                "https://github.com/genialis/resdk-scripts/tree/master/"
+                "BCM_project/upload_scripts."
+            )
+            if diff1:
+                raise NameError(
+                    err_msg.format(', '.join(diff1), "are missing")
+                    )
+            if diff2:
+                raise NameError(
+                    err_msg.format(', '.join(diff2), "not recognised")
+                    )
 
 
-parser = argparse.ArgumentParser(description='Upload raw data.')
+class Sample(object):
+    """Create a Sample like object."""
 
-parser.add_argument('-sample_sheet', type=str, help='Sample sheet', required=True)
-parser.add_argument('-username', type=str, help='Username', required=True)
-parser.add_argument('-password', type=str, help='Password', required=True)
-parser.add_argument('-URL', type=str, help='URL', required=True)
+    def __init__(self, sample):
+        self.name = sample['SAMPLE_NAME']
+        self.slug = sample['SAMPLE_NAME']
+        self.collection = sample['COLLECTION']
+        self.path = sample['FASTQ_R1']
+        self.path2 = sample['FASTQ_R2']
+        self.seq_type = sample['SEQ_TYPE']
+        self.reads_annotation = {
+            'experiment_type': self.seq_type,
+            'protocols': {
+                'extract_protocol': sample['EXTRACTION_PROTOCOL'],
+                'library_prep': sample['LIBRARY_CONSTRUCTION_PROTOCOL'],
+                'treatment_protocol': sample['TREATMENT_PROTOCOL'],
+                'growth_protocol': sample['GROWTH_PROTOCOL'],
+                'antibody_information': {
+                    'manufacturer' : sample['ANTIBODY']
+                }
+            },
+            'reads_info': {
+                'barcode': sample['BARCODE'],
+                'facility': sample['FACILITY']
+            }
+        }
+        self.molecule = sample['MOLECULE']
+        self.organism = sample['ORGANISM']
+        self.sample_annotation = {
+            'sample': {
+                'annotator': sample['ANNOTATOR'],
+                'cell_type': sample['CELL_TYPE'],
+                'source': sample['SOURCE'],
+                'strain': sample['STRAIN'],
+                'genotype': sample['GENOTYPE'],
+                'optional_char': []
+            }
+        }
+        if self.organism:
+            self.sample_annotation['sample']['organism'] = self.organism
+        if self.molecule:
+            self.sample_annotation['sample']['molecule'] = self.molecule
+        for option in OPTIONAL:
+            if sample[option]:
+                self.sample_annotation['sample']['optional_char'].append(
+                    '{0}:{1}'.format(option, sample[option]))
 
-args = parser.parse_args()
-
-res = resdk.Resolwe(args.username, args.password, args.URL)
-resdk.start_logging()
-
-samples = {}
-
-with open(args.sample_sheet, 'rb') as sample_sheet:
-    sample_reader = csv.reader(sample_sheet, delimiter='\t')
-    header = next(sample_reader)
-    for row in sample_reader:
-        samples[row[0]] = {col: '' for col in header}
-        for i, column in enumerate(row):
-            if i == 0:
-                continue  # skip sample name
-            samples[row[0]] [header[i]] = column
+    def tag_community(self):
+        """Prepare community tags."""
+        seq = self.seq_type.lower()
+        if 'rna' in seq:
+            community = 'community:rna-seq'
+        elif 'chip' in seq:
+            community = 'community:chip-seq'
+        else:
+            community = None
+        return community
 
 
-for s in samples:
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='Upload raw data.')
+    parser.add_argument('--sample_sheet', type=str, help='Sample sheet', required=True)
+    parser.add_argument('--username', type=str, help='Username', required=True)
+    parser.add_argument('--password', type=str, help='Password', required=True)
+    parser.add_argument('--URL', type=str, help='URL', required=True)
+    return parser.parse_args()
 
-    collection_name = samples[s]['COLLECTION']
-    try:
-        collection = res.collection.get(name=collection_name)
-    except LookupError:
-        collection = Collection(resolwe=res)
-        collection.name = collection_name
+
+def get_or_create_collection(resolwe, coll_name):
+    """Check if Collection with given name already exists. Create new Collection if not."""
+
+    collections = resolwe.collection.filter(name=coll_name)
+    if len(collections) > 1:
+        raise LookupError(
+            "More than one collection with name '{}' already exists on the platform!"
+            "".format(coll_name)
+        )
+
+    if not collections:
+        collection = resdk.resources.Collection(resolwe=resolwe)
+        collection.name = coll_name
         collection.save()
-
-    if samples[s]['PAIRED'] == '0':
-        src = samples[s]['FASTQ_R1'].split(",")
-        print('Uploading data for sample: {}'.format(s))
-        reads = res.run('upload-fastq-single',
-                        input={'src': src},
-                        collections=[collection.id])
-
-    elif samples[s]['PAIRED'] == '1':
-        src1 = samples[s]['FASTQ_R1'].strip(',').split(',')
-        src2 = samples[s]['FASTQ_R2'].strip(',').split(',')
-
-        print('Uploading data for sample: {}'.format(s))
-        reads = res.run('upload-fastq-paired',
-                        input={
-                            'src1': src1,
-                            'src2': src2},
-                        collections=[collection.id])
-
     else:
-        raise KeyError('Invalid PAIRED option.')
+        collection = resolwe.collection.get(name=coll_name)
 
-    reads_annotation = {
-        'experiment_type': samples[s]['SEQ_TYPE'],
-        'protocols': {
-            'extract_protocol': samples[s]['EXTRACTION_PROTOCOL'],
-            'library_prep': samples[s]['LIBRARY_CONSTRUCTION_PROTOCOL'],
-	    'treatment_protocol': samples[s]['TREATMENT_PROTOCOL'],
-	    'growth_protocol': samples[s]['GROWTH_PROTOCOL'],
-        }
-    }
+    return collection
 
-    reads.descriptor_schema = 'reads'
-    reads.descriptor = reads_annotation
-    reads.save()
 
-    # Get sample object
-    main_sample = reads.sample
+def main():
+    args = parse_arguments()
 
-    # rename a main_sample
-    main_sample.name = s
-    main_sample.slug = s
-    main_sample.save()
+    res = resdk.Resolwe(args.username, args.password, args.URL)
+    resdk.start_logging()
 
-    # provide sample annotation
-    sample_annotation = {
-        'sample': {
-            'annotator': samples[s]['ANNOTATOR'],
-            'source': samples[s]['SOURCE'],
-            'organism': samples[s]['ORGANISM'],
-            'strain': samples[s]['STRAIN'],
-            'genotype': samples[s]['GENOTYPE'],
-            'molecule': samples[s]['MOLECULE'],
-            'optional_char': []
-        }
-    }
+    # Read  and validate the annotation template
+    annotation = FileImporter(args.sample_sheet)
 
-    if samples[s]['LIBRARY_STRATEGY']:
-        sample_annotation['sample']['optional_char'].append(
-            'LIBRARY_STRATEGY:{}'.format(samples[s]['LIBRARY_STRATEGY']))
+    for sample in annotation.sample_list:
+        read_file = Sample(sample)
 
-    if samples[s]['TISSUE']:
-        sample_annotation['sample']['optional_char'].append(
-            'TISSUE:{}'.format(samples[s]['TISSUE']))
+        # Create or get a collection if existing
+        coll = read_file.collection
+        if coll:
+            collection = get_or_create_collection(res, coll)
 
-    if samples[s]['AGE']:
-        sample_annotation['sample']['optional_char'].append(
-            'AGE:{}'.format(samples[s]['AGE']))
+        path = read_file.path
+        path2 = read_file.path2
+        err_path = []
+        if path and path2:
+            src1 = path.split(',')
+            src2 = path2.split(',')
+            err_path = [s for s in src1 + src2 if not glob.glob(s)]
+            if err_path:
+                print("Incorrect file paths: {}".format(err_path))
+                continue
+            print('Uploading data for sample: {}'.format(read_file.name))
+            reads = res.run('upload-fastq-paired',
+                            input={
+                                'src1': src1,
+                                'src2': src2})
 
-     if samples[s]['OTHER_CHAR_1']:
-        sample_annotation['sample']['optional_char'].append(
-            'OTHER_CHAR_1:{}'.format(samples[s]['OTHER_CHAR_1']))
+        elif path and not path2:
+            src = path.split(',')
+            err_path = [s for s in src if not glob.glob(s)]
+            if err_path:
+                print("Incorrect file paths: {}".format(err_path))
+                continue
+            print('Uploading data for sample: {}'.format(read_file.name))
+            reads = res.run('upload-fastq-single',
+                            input={'src': src})
 
-     if samples[s]['OTHER_CHAR_2']:
-        sample_annotation['sample']['optional_char'].append(
-            'OTHER_CHAR_2:{}'.format(samples[s]['OTHER_CHAR_2']))
+        else:
+            print("Warning: There are no file paths or there is only "
+                  "the reverse reads path provided for sample {} so "
+                  "it was not uploaded. You should fix or provide the "
+                  "correct file paths".format(read_file.name))
+            continue
 
-    main_sample.descriptor = sample_annotation
-    main_sample.save()
+        if coll:
+            collection.add_data(reads)
 
-    # confirm that the sample is annotated and attach it to the collection
-    main_sample.confirm_is_annotated()
-    collection.add_samples(main_sample)
+        # Rename reads object
+        reads.name = read_file.name
+        reads.slug = read_file.slug
 
+        # Provide reads annotation
+        reads.descriptor_schema = 'reads'
+        reads.descriptor = read_file.reads_annotation
+        reads.save()
+
+        # Get sample object
+        main_sample = reads.sample
+
+        # Rename main_sample
+        main_sample.name = read_file.name
+        main_sample.slug = read_file.slug
+
+        # Provide sample annotation and tag community
+        main_sample.descriptor_schema = 'sample'
+        main_sample.descriptor = read_file.sample_annotation
+        if read_file.tag_community():
+            main_sample.tags.append(read_file.tag_community())
+        main_sample.save()
+
+        # Confirm that the sample is annotated and attach it to the collection
+        main_sample.confirm_is_annotated()
+        if coll:
+            collection.add_samples(main_sample)
+
+if __name__ == "__main__":
+    main()
